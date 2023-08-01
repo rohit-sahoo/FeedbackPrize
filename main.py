585e3e050866f8e0897cc737bb30508acdf027ca
PratikHotchandani <pratikhot123@live.com>
2023-08-01 16:55:49 -0400

    updated requirements.txt file

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import streamlit as st
+import pandas as pd
+import os
+from pathlib import Path
+import spacy
+from spacy import displacy
+
 
 st.markdown("<h1><em>ArguSense: Intelligent NLP-driven Argument Evaluation and Effectiveness Analysis</em></h1>", unsafe_allow_html=True)
 
 st.subheader("Enter your prompt in the textbox below")
 
 # Add a textbox to the app
-user_input = st.text_input("Enter your name:", "John Doe")
+user_input = st.text_area("Enter your text:", "Default text", height=400)
 
 if st.button("Proceed"):
         # Display the input
