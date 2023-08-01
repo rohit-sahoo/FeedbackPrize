1677108206663c2cae170d3e89351b6f5e5fe3b9
PratikHotchandani <pratikhot123@live.com>
2023-08-01 16:41:37 -0400

    added button and heading

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import streamlit as st
 
+st.markdown("<h1><em>ArguSense: Intelligent NLP-driven Argument Evaluation and Effectiveness Analysis</em></h1>", unsafe_allow_html=True)
 
 st.subheader("Enter your prompt in the textbox below")
 
 # Add a textbox to the app
 user_input = st.text_input("Enter your name:", "John Doe")
 
-# Display the input
-st.write(f"Hello, {user_input}!")
+if st.button("Proceed"):
+        # Display the input
+        st.write(f"Hello, {user_input}!")
