831d994eb41b29989fd3772ae568baa053dd5ea2
PratikHotchandani <pratikhot123@live.com>
2023-08-01 16:31:54 -0400

    added textbox

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ import streamlit as st
 
 
 st.subheader("Enter your prompt in the textbox below")
+
+# Add a textbox to the app
+user_input = st.text_input("Enter your name:", "John Doe")
+
+# Display the input
+st.write(f"Hello, {user_input}!")
