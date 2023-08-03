88593746860902c51247cea13bde948a8f7370c2
PratikHotchandani <pratikhot123@live.com>
2023-08-02 23:42:17 -0400

    tokens targets and attention added

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ from pathlib import Path
 import spacy
 from spacy import displacy
 
+LOAD_MODEL_FROM = 'long_v14.h5'
+
+# LOAD MODEL
+if LOAD_MODEL_FROM:
+    model.load_weights(f'{LOAD_MODEL_FROM}/long_v{VER}.h5')
+    
 
 st.markdown("<h1><em>ArguSense: Intelligent NLP-driven Argument Evaluation and Effectiveness Analysis</em></h1>", unsafe_allow_html=True)
 
@@ -13,49 +19,6 @@ st.subheader("Enter your prompt in the textbox below")
 
 # Add a textbox to the app
 user_input = st.text_area("Enter your text:", "Default text", height=400)
-
-
-"""
-
-
-colors = {
-            'Lead': '#8000ff',
-            'Position': '#2b7ff6',
-            'Evidence': '#2adddd',
-            'Claim': '#80ffb4',
-            'Concluding Statement': 'd4dd80',
-            'Counterclaim': '#ff8042',
-            'Rebuttal': '#ff0000'
-         }
-
-
-discourse_type = ['Lead',
- 'Position',
- 'Evidence',
- 'Claim',
- 'Concluding Statement',
- 'Counterclaim',
- 'Rebuttal']
-
-def visualize(example):
-    ents = []
-    for i, row in train[train['id'] == example].iterrows():
-        ents.append({
-                        'start': int(row['discourse_start']), 
-                        'end': int(row['discourse_end']), 
-                        'label': row['discourse_type']
-                    })
-
-    doc2 = {
-        "text": example,
-        "ents": ents,
-        "title": "Your passage"
-    }
-
-    options = {"ents": discourse_type, "colors": colors}
-    displacy.render(doc2, style="ent", options=options, manual=True, jupyter=True)
-
-"""
 
 
 def save_file_to_test(example):
